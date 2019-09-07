c6f15c08fe37d2d3e623911fbe27a3d6279c775a
vBoBv <vannponhvath@gmail.com>
2019-09-07 14:04:41 +1200

    Updates

---
+++ precache-manifest.097a31586ba4b0b0629b417073bad6d6.js
@@ -0,0 +1,206 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebb57739358b04293eda197761b6aa19",
+    "url": "/ponhvathvann/index.html"
+  },
+  {
+    "revision": "f606e17f79d56b8b896c",
+    "url": "/ponhvathvann/static/css/2.539bd89c.chunk.css"
+  },
+  {
+    "revision": "076d00ce4220f4659276",
+    "url": "/ponhvathvann/static/css/main.df241dd5.chunk.css"
+  },
+  {
+    "revision": "f606e17f79d56b8b896c",
+    "url": "/ponhvathvann/static/js/2.e08a19de.chunk.js"
+  },
+  {
+    "revision": "076d00ce4220f4659276",
+    "url": "/ponhvathvann/static/js/main.0af9b1c1.chunk.js"
+  },
+  {
+    "revision": "800d0c01ae0730abddb5",
+    "url": "/ponhvathvann/static/js/runtime~main.50bf8f7b.js"
+  },
+  {
+    "revision": "39b2c3031be6b4ea96e2e3e95d307814",
+    "url": "/ponhvathvann/static/media/Roboto-Bold.39b2c303.woff2"
+  },
+  {
+    "revision": "dc81817def276b4f21395f7ea5e88dcd",
+    "url": "/ponhvathvann/static/media/Roboto-Bold.dc81817d.woff"
+  },
+  {
+    "revision": "e31fcf1885e371e19f5786c2bdfeae1b",
+    "url": "/ponhvathvann/static/media/Roboto-Bold.e31fcf18.ttf"
+  },
+  {
+    "revision": "ecdd509cadbf1ea78b8d2e31ec52328c",
+    "url": "/ponhvathvann/static/media/Roboto-Bold.ecdd509c.eot"
+  },
+  {
+    "revision": "3b813c2ae0d04909a33a18d792912ee7",
+    "url": "/ponhvathvann/static/media/Roboto-Light.3b813c2a.woff"
+  },
+  {
+    "revision": "46e48ce0628835f68a7369d0254e4283",
+    "url": "/ponhvathvann/static/media/Roboto-Light.46e48ce0.ttf"
+  },
+  {
+    "revision": "69f8a0617ac472f78e45841323a3df9e",
+    "url": "/ponhvathvann/static/media/Roboto-Light.69f8a061.woff2"
+  },
+  {
+    "revision": "a990f611f2305dc12965f186c2ef2690",
+    "url": "/ponhvathvann/static/media/Roboto-Light.a990f611.eot"
+  },
+  {
+    "revision": "4d9f3f9e5195e7b074bb63ba4ce42208",
+    "url": "/ponhvathvann/static/media/Roboto-Medium.4d9f3f9e.eot"
+  },
+  {
+    "revision": "574fd0b50367f886d359e8264938fc37",
+    "url": "/ponhvathvann/static/media/Roboto-Medium.574fd0b5.woff2"
+  },
+  {
+    "revision": "894a2ede85a483bf9bedefd4db45cdb9",
+    "url": "/ponhvathvann/static/media/Roboto-Medium.894a2ede.ttf"
+  },
+  {
+    "revision": "fc78759e93a6cac50458610e3d9d63a0",
+    "url": "/ponhvathvann/static/media/Roboto-Medium.fc78759e.woff"
+  },
+  {
+    "revision": "2751ee43015f9884c3642f103b7f70c9",
+    "url": "/ponhvathvann/static/media/Roboto-Regular.2751ee43.woff2"
+  },
+  {
+    "revision": "30799efa5bf74129468ad4e257551dc3",
+    "url": "/ponhvathvann/static/media/Roboto-Regular.30799efa.eot"
+  },
+  {
+    "revision": "ba3dcd8903e3d0af5de7792777f8ae0d",
+    "url": "/ponhvathvann/static/media/Roboto-Regular.ba3dcd89.woff"
+  },
+  {
+    "revision": "df7b648ce5356ea1ebce435b3459fd60",
+    "url": "/ponhvathvann/static/media/Roboto-Regular.df7b648c.ttf"
+  },
+  {
+    "revision": "7500519de3d82e33d1587f8042e2afcb",
+    "url": "/ponhvathvann/static/media/Roboto-Thin.7500519d.woff"
+  },
+  {
+    "revision": "94998475f6aea65f558494802416c1cf",
+    "url": "/ponhvathvann/static/media/Roboto-Thin.94998475.ttf"
+  },
+  {
+    "revision": "954bbdeb86483e4ffea00c4591530ece",
+    "url": "/ponhvathvann/static/media/Roboto-Thin.954bbdeb.woff2"
+  },
+  {
+    "revision": "dfe56a876d0282555d1e2458e278060f",
+    "url": "/ponhvathvann/static/media/Roboto-Thin.dfe56a87.eot"
+  },
+  {
+    "revision": "11911410dca2de148f30954eb2fd5eab",
+    "url": "/ponhvathvann/static/media/arrow_left.11911410.svg"
+  },
+  {
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "/ponhvathvann/static/media/arrow_right.8ef6a08c.svg"
+  },
+  {
+    "revision": "7640112ef266b06d3a3b3dcf2e8979e3",
+    "url": "/ponhvathvann/static/media/bankApp.7640112e.gif"
+  },
+  {
+    "revision": "0aad8279cab6ce689f53e493956cbf92",
+    "url": "/ponhvathvann/static/media/cms.0aad8279.gif"
+  },
+  {
+    "revision": "f17a9ea40e286e769a581ecd58d598c6",
+    "url": "/ponhvathvann/static/media/consoleGame.f17a9ea4.gif"
+  },
+  {
+    "revision": "14c590d124662eb50efa4c00f027b79c",
+    "url": "/ponhvathvann/static/media/fa-brands-400.14c590d1.eot"
+  },
+  {
+    "revision": "3e1b2a654a784ceb385157140b4ccd71",
+    "url": "/ponhvathvann/static/media/fa-brands-400.3e1b2a65.woff2"
+  },
+  {
+    "revision": "5e8aa9ea0ebcd2218178f554cdd8e6b0",
+    "url": "/ponhvathvann/static/media/fa-brands-400.5e8aa9ea.ttf"
+  },
+  {
+    "revision": "bfa9c38bd6081dafe7278dedc4aad1d9",
+    "url": "/ponhvathvann/static/media/fa-brands-400.bfa9c38b.svg"
+  },
+  {
+    "revision": "df02c782834b113d605d8329368737b4",
+    "url": "/ponhvathvann/static/media/fa-brands-400.df02c782.woff"
+  },
+  {
+    "revision": "285a9d2a28886ad64c4e45cbd733cf7c",
+    "url": "/ponhvathvann/static/media/fa-regular-400.285a9d2a.ttf"
+  },
+  {
+    "revision": "5623624dd1b017c66c29d1ac69cdcfa3",
+    "url": "/ponhvathvann/static/media/fa-regular-400.5623624d.woff"
+  },
+  {
+    "revision": "95f13e0be408d117bd3d9b366084a3ef",
+    "url": "/ponhvathvann/static/media/fa-regular-400.95f13e0b.svg"
+  },
+  {
+    "revision": "aa66d0e0e38c75666e98db33abae955e",
+    "url": "/ponhvathvann/static/media/fa-regular-400.aa66d0e0.eot"
+  },
+  {
+    "revision": "ac21cac3f22cc9642f5af32e0c750797",
+    "url": "/ponhvathvann/static/media/fa-regular-400.ac21cac3.woff2"
+  },
+  {
+    "revision": "3ded831d708bf90b2da86756551b5c1c",
+    "url": "/ponhvathvann/static/media/fa-solid-900.3ded831d.woff"
+  },
+  {
+    "revision": "42e1fbd2cf655a0b44a2dfae9ca2f8c1",
+    "url": "/ponhvathvann/static/media/fa-solid-900.42e1fbd2.eot"
+  },
+  {
+    "revision": "6ed5e3bc9018d2a03e2f1fadbf8a0a4a",
+    "url": "/ponhvathvann/static/media/fa-solid-900.6ed5e3bc.svg"
+  },
+  {
+    "revision": "896e20e26ad07dc63c9526ed814117e9",
+    "url": "/ponhvathvann/static/media/fa-solid-900.896e20e2.ttf"
+  },
+  {
+    "revision": "d6d8d5da9214dc7d46b297672a602d55",
+    "url": "/ponhvathvann/static/media/fa-solid-900.d6d8d5da.woff2"
+  },
+  {
+    "revision": "f863778cf0321bf496628cb8146241ea",
+    "url": "/ponhvathvann/static/media/ponhvathvannCV.f863778c.pdf"
+  },
+  {
+    "revision": "5d1f4307d6ebba4d2bd195a2606fb020",
+    "url": "/ponhvathvann/static/media/travelBuddy.5d1f4307.gif"
+  },
+  {
+    "revision": "92c75fba664903b0c7197ab9e6392893",
+    "url": "/ponhvathvann/static/media/videoApp.92c75fba.gif"
+  },
+  {
+    "revision": "0babc688afb262ac6b9799728fa4c477",
+    "url": "/ponhvathvann/static/media/wall.0babc688.jpg"
+  },
+  {
+    "revision": "85b7f883ed20c694a2d1a9fbea34e4ae",
+    "url": "/ponhvathvann/static/media/website.85b7f883.gif"
+  }
+]);
