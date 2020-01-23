5ec1bd37d2f895c30b216f10c87b786fbf426042
vBoBv <vannponhvath@gmail.com>
2020-01-24 10:47:38 +1300

    update skills and cv

--- src/reducers/skillsReducer.js
+++ src/reducers/skillsReducer.js
@@ -1,28 +1,54 @@
-import React from 'react';
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { fab } from '@fortawesome/free-brands-svg-icons'
-import { library } from '@fortawesome/fontawesome-svg-core'
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import { library } from "@fortawesome/fontawesome-svg-core";
 
-library.add(
-    fab
-)
+library.add(fab);
 
-const pythonIcon = <FontAwesomeIcon icon={["fab","python"]} />
-const javaIcon = <FontAwesomeIcon icon={["fab","java"]} />
-const htmlIcon = <FontAwesomeIcon icon={["fab","html5"]} />
-const cssIcon = <FontAwesomeIcon icon={["fab","css3"]} />
-const javascriptIcon = <FontAwesomeIcon icon={["fab","js"]} />
-const bootstrapIcon = <FontAwesomeIcon icon={["fab","bootstrap"]} />
-const reactIcon = <FontAwesomeIcon icon={["fab","react"]} />
+const pythonIcon = <FontAwesomeIcon icon={["fab", "python"]} />;
+const javaIcon = <FontAwesomeIcon icon={["fab", "java"]} />;
+const htmlIcon = <FontAwesomeIcon icon={["fab", "html5"]} />;
+const cssIcon = <FontAwesomeIcon icon={["fab", "css3"]} />;
+const javascriptIcon = <FontAwesomeIcon icon={["fab", "js"]} />;
+const bootstrapIcon = <FontAwesomeIcon icon={["fab", "bootstrap"]} />;
+const reactIcon = <FontAwesomeIcon icon={["fab", "react"]} />;
 
 const skillsReducer = () => {
     return [
-        { category: 'Programming', skillList: ['C','Java', 'Python'], icon: [ javaIcon, pythonIcon ] },
-        { category: 'Frameworks', skillList: ['React', 'Redux', 'React Native', 'Bootstrap 4'] },
-        { category: 'Front-End & Back-End', skillList: ['HTML5', 'CSS3', 'JavaScript (ES6)', 'Node', 'Express', 'MongoDB'], icon: [ htmlIcon, cssIcon, javascriptIcon, bootstrapIcon, reactIcon] },
+        {
+            category: "Programming",
+            skillList: ["C", "Java", "Python"],
+            icon: [javaIcon, pythonIcon]
+        },
+        {
+            category: "Frameworks",
+            skillList: ["React", "Redux", "React Native", "Bootstrap 4", "Sass"]
+        },
+        {
+            category: "Front-End & Back-End",
+            skillList: [
+                "HTML5",
+                "CSS3",
+                "JavaScript (ES6)",
+                "Node",
+                "Express",
+                "MongoDB"
+            ],
+            icon: [htmlIcon, cssIcon, javascriptIcon, bootstrapIcon, reactIcon]
+        },
         // { category: 'OS Management', skillList: ['Debian', 'Raspbian', 'Hardware Technology', 'Basic Network-Configuration'] },
-        { category: 'Additional Skills', skillList: ['GitHub', 'Git / Git Flow', 'Wireframing / UML', 'Microsoft SQL Server', 'SDLC', 'Linux / Debian / Raspbian'] }
-    ]
-}
+        {
+            category: "Additional Skills",
+            skillList: [
+                "GitHub",
+                "Git / Git Flow",
+                "Wireframing / UML",
+                "Microsoft SQL Server",
+                "SDLC",
+                "Linux / Debian / Raspbian"
+            ]
+        }
+    ];
+};
 
 export default skillsReducer;
